c0090f68399a9041dd70ee704f8c50aebaec5018
Humberto <hdkaratechop@gmail.com>
2023-01-06 20:30:27 -0800

    add create function to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
-    def new
-    end
+    def create
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      
+        if @user.save
+          redirect_to new_user_path
+        else
+        #   render :new, status: :unprocessable_entity
 
-    def create 
-    end
+        end
 
-end
+      end
+
+      private
+      # returns the hash containing the params we need
+      # to create or update a post
+      def user_params
+        params.require(:users).permit(:username,:email,:password)
+      end
+    end
+    
+
+
+
