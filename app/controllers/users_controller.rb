372ad602c5ea6cf172b329e862a953dded069e65
Humberto <hdkaratechop@gmail.com>
2023-01-07 15:30:09 -0800

    add the edit and update actions to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
-class UsersController < ApplicationController
-    def create
+class UsersController < ApplicationController 
+  def new
+    @user = User.new
+  end
+
+  def create
         @user = User.new(user_params)
       
         if @user.save
@@ -8,16 +12,32 @@ class UsersController < ApplicationController
           render :new
 
         end
-
       end
 
-      private
-      # returns the hash containing the params we need
-      # to create or update a post
-      def user_params
-        params.require(:users).permit(:username,:email,:password)
-      end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
     end
+  end
+
+
+  private
+  # returns the hash containing the params we need
+  # to create or update a post
+  def user_params
+    params.require(:users).permit(:username,:email,:password)
+  end
+end
+
+
 
 
 
