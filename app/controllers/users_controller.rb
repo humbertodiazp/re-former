9fd7344693edd763299da394d44fac4286690e65
Humberto <hdkaratechop@gmail.com>
2023-01-06 22:26:33 -0800

    pass user user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
       
         if @user.save
           redirect_to new_user_path
         else
-        #   render :new, status: :unprocessable_entity
+          render :new
 
         end
 
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
         params.require(:users).permit(:username,:email,:password)
       end
     end
-    
+
 
 
 
